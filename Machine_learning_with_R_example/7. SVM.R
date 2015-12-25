28fe52efc0c18b1806caf3fcc292dab3d5c47602
Richard <richarddeng88@gmail.com>
2015-12-25 17:02:42 -0500

    SVM

--- Machine_learning_with_R_example/7. SVM.R
+++ Machine_learning_with_R_example/7. SVM.R
@@ -1,1 +1,23 @@
-data <- read.csv("data/Machine-Learning-with-R-datasets/letterdata.csv")
+letters <- read.csv("data/Machine-Learning-with-R-datasets/letterdata.csv")
+letters_train <- letters[1:16000, ]
+letters_test <- letters[16001:20000, ]
+
+# TRAIN THE MODEL
+library(kernlab)
+letter_classifier <- ksvm(letter ~ ., data = letters_train, kernel = "vanilladot")
+letter_classifier
+
+# EVALUATING PERFORMANCE
+letter_predictions <- predict(letter_classifier, letters_test)
+table(letter_predictions, letters_test$letter)
+
+agreement <- letter_predictions == letters_test$letter
+table(agreement)
+prop.table(table(agreement))
+
+# IMPROVE THE PERFORMANCE
+letter_classifier_rbf <- ksvm(letter ~ ., data = letters_train,kernel = "rbfdot")
+letter_predictions_rbf <- predict(letter_classifier_rbf,letters_test)
+agreement_rbf <- letter_predictions_rbf == letters_test$letter
+table(agreement_rbf)
+prop.table(table(agreement_rbf))
